b7ed06119fef70402d2003273c2e79c12978a3cb
Seth Thompson <s3th.thompson@gmail.com>
2019-08-22 11:38:01 -0400

    fix link

--- api/donate.js
+++ api/donate.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
   const url = `https://secure.actblue.com/donate/dec-dc-action`
 
-  console.log(`[${Date.time()}] User clicked donate link`)
+  console.log(`[${new Date()}] User clicked donate link`)
 
   let str = "redirecting..."
   res.writeHead(302, {
